ae0c3338e2e51eae0623c277a3d9fdf1e65c6491
Walter Chavarria Mora <wchavarria03@gmail.com>
2017-05-14 23:42:40 -0600

    Updating for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 8080;
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -8,4 +9,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
